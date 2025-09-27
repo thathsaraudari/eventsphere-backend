000d0b4b996fcdfa1258b937600fa4afe6575787
thathsaraudari <105314224+thathsaraudari@users.noreply.github.com>
2025-09-27 11:10:13 +0200

    Implement myEvents routes

--- middlewares/jwt.middleware.js
+++ middlewares/jwt.middleware.js
@@ -1,21 +1,28 @@
 const jwt = require("jsonwebtoken");
+const User = require('../models/User.model');
 
-function isAuthenticated(req, res, next) {
+async function isAuthenticated(req, res, next) {
     try {
         if (
-            req.headers.authotization.split(" ")[0] === "Bearer" &&
-            req.hearders.authotization.split(" ")[1]
+            req.headers.authorization.split(" ")[0] === "Bearer" &&
+            req.headers.authorization.split(" ")[1]
         ) {
-            const theToken = req.hearders.authotization.split(" ")[1];
-            const theData = jwt.verify(theToken, process.env.TOKEN_SECRET);
-            req.hearders.payload = theData;
+            const theToken = req.headers.authorization.split(" ")[1];
+            const decoded = jwt.verify(theToken, process.env.TOKEN_SECRET);
+            const user = await User.findById(decoded.id);
+            if (!user) {
+                res.status(401).json({ errorMessage: "User not found" });
+                return;
+            }
+            req.user = user;
             next();
         } else {
             req.status(400).json( { errorMessage: "no token"});
         } 
   } catch (error) {
-            res.status(403).json({ errorMessage: "Invalid Token"});
-        }
+        console.log(error);
+        res.status(403).json({ errorMessage: "Invalid Token" });
+    }
 }
 
-modile.exports = { isAuthenticated };
+module.exports = { isAuthenticated };
